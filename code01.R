a049c1306c60fc04083c45eddce0fe8a851b5016
Ramon Pio Roda <ramsharj@gmail.com>
2018-03-25 21:30:30 +0800

    Updated

---
+++ code01.R
@@ -0,0 +1,86 @@
+#eda
+getwd()
+setwd("E:/Other Business/RamonPioRoda/eda/")
+getwd()
+
+## Download File
+URL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile = "household_power_consumption.zip"
+download.file(URL, destfile)
+
+dir()
+
+######
+unzip(destfile)
+dir()
+
+################ Plot 1
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+head(subSetData)
+str(subSetData)
+nrow(subSetData)
+
+
+#str(subSetData)
+gap <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=500, height=500)
+hist(gap, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
+############### Plot 2
+
+head(subSetData)
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+gap <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=500, height=500)
+plot(datetime, gap, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+
+############### Plot 3
+
+head(subSetData)
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+gap <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+png("plot3.png", width=500, height=500)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
+
+################# Plot 4
+head(subSetData)
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+gap <- as.numeric(subSetData$Global_active_power)
+grp <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+
+
+png("plot4.png", width=500, height=500)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, gap, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, grp, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
